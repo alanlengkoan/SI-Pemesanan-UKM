083bf60f36b181e39d6dc08f957abea125fc26a2
alanlengkoan <alansaputralengkoan@gmail.com>
2022-07-24 15:08:45 +0800

    update

--- configs/koneksi.php
+++ configs/koneksi.php
@@ -1,12 +1,13 @@
 <?php
 
 // untuk koneksi ke database
-$host   = "localhost";
+$host   = "127.0.0.1";
 $dbuser = "my_root";
 $dbpass = "my_pass";
 $dbname = "si_pemesananukm";
+$port   = "3307";
 
-$koneksi = new mysqli ($host, $dbuser, $dbpass, $dbname);
+$koneksi = new mysqli($host, $dbuser, $dbpass, $dbname, $port);
 
 if ($koneksi->connect_errno) {
     echo "Gagal Koneksi " . $koneksi->connect_error;
